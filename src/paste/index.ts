16e10dc4df8d7169d66e202a9adecbf9e94470e4
Jee Wan Gue <jeewangue@gmail.com>
2023-02-26 22:36:55 +0900

    darwin support

--- src/paste/index.ts
+++ src/paste/index.ts
@@ -1,5 +1,7 @@
+import os from 'os';
 import { compact, flow, includes, pullAll, split } from 'lodash/fp';
 import { createSingleton } from '../util';
+import { logger } from '../common/logger';
 
 const zx = createSingleton('zx', async () => import('zx'));
 
@@ -23,29 +25,56 @@ export const getFormattedClipboard = async (
   const { $ } = await zx.get();
   $.verbose = false;
 
-  const targets = flow(
-    split('\n'),
-    compact,
-    pullAll(xclipSpecialTargets),
-  )((await $`xclip -o -selection ${selection} -target TARGETS`).stdout);
+  if (os.platform() === 'linux') {
+    const targets = flow(
+      split('\n'),
+      compact,
+      pullAll(xclipSpecialTargets),
+    )((await $`xclip -o -selection ${selection} -target TARGETS`).stdout);
 
-  if (to === FormatTo.Gfm) {
-    if (includes('text/html')(targets)) {
-      return $`xclip -o -selection ${selection} -target text/html`
-        .pipe($`pandoc --from=html --to=gfm-raw_html --wrap=none`)
-        .then((s) => s.stdout);
-    } else {
-      return $`xclip -o -selection ${selection}`
-        .pipe($`pandoc --from=html --to=gfm-raw_html --wrap=none`)
-        .then((s) => s.stdout);
-    }
-  } else if (to === FormatTo.Html) {
-    if (includes('text/html')(targets)) {
-      return $`xclip -o -selection ${selection} -target text/html`.then((s) => s.stdout);
+    if (to === FormatTo.Gfm) {
+      if (includes('text/html')(targets)) {
+        return $`xclip -o -selection ${selection} -target text/html`
+          .pipe($`pandoc --from=html --to=gfm-raw_html --wrap=none`)
+          .then((s) => s.stdout);
+      } else {
+        return $`xclip -o -selection ${selection}`
+          .pipe($`pandoc --from=html --to=gfm-raw_html --wrap=none`)
+          .then((s) => s.stdout);
+      }
+    } else if (to === FormatTo.Html) {
+      if (includes('text/html')(targets)) {
+        return $`xclip -o -selection ${selection} -target text/html`.then((s) => s.stdout);
+      } else {
+        return $`xclip -o -selection ${selection}`.then((s) => s.stdout);
+      }
     } else {
       return $`xclip -o -selection ${selection}`.then((s) => s.stdout);
     }
+  } else if (os.platform() === 'darwin') {
+    if (to === FormatTo.Gfm) {
+      try {
+        return $`osascript -e 'the clipboard as «class HTML»' | perl -ne 'print chr foreach unpack("C*",pack("H*",substr($_,11,-3)))'`
+          .pipe($`pandoc --from=html --to=gfm-raw_html --wrap=none`)
+          .then((s) => s.stdout);
+      } catch (err) {
+        logger.error(err);
+        return '';
+      }
+    } else if (to === FormatTo.Html) {
+      try {
+        return $`osascript -e 'the clipboard as «class HTML»' | perl -ne 'print chr foreach unpack("C*",pack("H*",substr($_,11,-3)))'`.then(
+          (s) => s.stdout,
+        );
+      } catch (err) {
+        logger.error(err);
+        return '';
+      }
+    } else {
+      return $`pbpaste`.then((s) => s.stdout);
+    }
   } else {
-    return $`xclip -o -selection ${selection}`.then((s) => s.stdout);
+    logger.error(`${os.platform()} is not supported`);
+    return '';
   }
 };
